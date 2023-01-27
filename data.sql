1480c248c48dc09f4f2ec253d114008db9627530
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-26 22:39:31 -0500

    Add star table

--- data.sql
+++ data.sql
@@ -22,3 +22,28 @@ VALUES ('Milky Way', 'The galaxy that contains our solar system', 13.5, 100),
        ('Maffei 2', 'A barred spiral galaxy located 8 million light-years away', 10.5, 90),
        ('Large Magellanic Cloud', 'A irregular galaxy located 160,000 light-years away', 2.5, 30),
        ('Small Magellanic Cloud', 'A irregular galaxy located 200,000 light-years away', 3.5, 35);
+
+/* Insert data into star table */
+
+INSERT INTO star (name, galaxy_id, description, has_life, is_spherical)
+VALUES ('Sun', 1, 'A G-type main-sequence star located in the Milky Way galaxy', false, true),
+       ('Proxima Centauri', 2, 'A red dwarf star located in the Andromeda galaxy', false, true),
+       ('Alpha Centauri A', 2, 'A G-type main-sequence star located in the Andromeda galaxy', false, true),
+       ('Alpha Centauri B', 2, 'A K-type main-sequence star located in the Andromeda galaxy', false, true),
+       ('Betelgeuse', 3, 'A red supergiant star located in the Whirlpool galaxy', false, false),
+       ('Antares', 4, 'A red supergiant star located in the Sombrero galaxy', false, false),
+       ('Deneb', 5, 'A blue supergiant star located in the Triangulum galaxy', false, false),
+       ('Vega', 6, 'A white main-sequence star located in the Pinwheel galaxy', false, true),
+       ('Sirius', 7, 'A white main-sequence star located in the Tadpole galaxy', false, true),
+       ('Arcturus', 8, 'A orange giant star located in the Cartwheel galaxy', false, true),
+       ('Rigel', 9, 'A blue supergiant star located in the Cigar galaxy', false, false),
+       ('Betelgeuse', 10, 'A red supergiant star located in the M101 galaxy', false, false),
+       ('Polaris', 11, 'A yellow supergiant star located in the M51 galaxy', false, true),
+       ('Deneb', 12, 'A blue supergiant star located in the Barred Spiral galaxy', false, false),
+       ('Regulus', 13, 'A blue main-sequence star located in the M104 galaxy', false, true),
+       ('Canopus', 14, 'A white giant star located in the M83 galaxy', false, true),
+       ('Achernar', 15, 'A blue main-sequence star located in the M77 galaxy', false, true),
+       ('Capella', 16, 'A yellow giant star located in the Maffei galaxy', false, true),
+       ('Pollux', 17, 'An orange giant star located in the Maffei 2 galaxy', false, true),
+       ('47 Tucanae', 18, 'A globular cluster located in the Large Magellanic Cloud galaxy', false, false),
+       ('NGC 292', 19, 'An open cluster located in the Small Magellanic Cloud galaxy', false, false);
--- schema.sql
+++ schema.sql
@@ -13,3 +13,14 @@ number_of_planets INT
 ALTER TABLE planet ADD CONSTRAINT planet_galaxy_id_fkey FOREIGN KEY (galaxy_id) REFERENCES galaxy(galaxy_id) ON DELETE CASCADE;
 ALTER TABLE star ADD CONSTRAINT star_galaxy_id_fkey FOREIGN KEY (galaxy_id) REFERENCES galaxy(galaxy_id) ON DELETE CASCADE;
 
+/* Create star table */
+
+CREATE TABLE star (
+star_id SERIAL PRIMARY KEY,
+name VARCHAR(200),
+galaxy_id INTEGER,
+description TEXT,
+has_life BOOLEAN,
+is_spherical BOOLEAN,
+FOREIGN KEY (galaxy_id) REFERENCES galaxy(galaxy_id) ON DELETE CASCADE
+);
