98a9e26cd2335e9bea0094a164d72fc1d0c74dc1
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-26 21:33:02 -0500

    create table galaxy and add information

--- data.sql
+++ data.sql
@@ -1,5 +1,24 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+/* Insert data into galxy table */
+
+INSERT INTO galaxy (name, description, age_in_millions_of_years, number_of_planets)
+VALUES ('Milky Way', 'The galaxy that contains our solar system', 13.5, 100),
+       ('Andromeda', 'A spiral galaxy located 2.5 million light-years away', 9.8, 120),
+       ('Whirlpool', 'A grand design spiral galaxy located 31 million light-years away', 10.5, 80),
+       ('Sombrero', 'An unbarred spiral galaxy located 28 million light-years away', 12.3, 150),
+       ('Triangulum', 'A spiral galaxy located 3 million light-years away', 8.5, 90),
+       ('Pinwheel', 'A grand design spiral galaxy located 21 million light-years away', 11.2, 110),
+       ('Tadpole', 'A spiral galaxy with a long tail located 420 million light-years away', 13.8, 140),
+       ('Cartwheel', 'A lenticular galaxy located 500 million light-years away', 14.6, 130),
+       ('Cigar', 'A barred spiral galaxy located 55 million light-years away', 10.1, 100),
+       ('M101', 'A spiral galaxy located 21 million light-years away', 10.9, 120),
+       ('M51', 'A grand design spiral galaxy located 23 million light-years away', 11.5, 115),
+       ('Barred Spiral', 'A barred spiral galaxy located 35 million light-years away', 12.2, 125),
+       ('M104', 'A spiral galaxy located 29 million light-years away', 11.3, 110),
+       ('M83', 'A barred spiral galaxy located 15 million light-years away', 12.4, 130),
+       ('M77', 'A spiral galaxy located 47 million light-years away', 12.7, 120),
+       ('Maffei', 'A barred spiral galaxy located 10 million light-years away', 11.1, 100),
+       ('Maffei 2', 'A barred spiral galaxy located 8 million light-years away', 10.5, 90),
+       ('Large Magellanic Cloud', 'A irregular galaxy located 160,000 light-years away', 2.5, 30),
+       ('Small Magellanic Cloud', 'A irregular galaxy located 200,000 light-years away', 3.5, 35);
--- schema.sql
+++ schema.sql
@@ -1,5 +1,15 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+/* Create galaxy table */
+
+CREATE TABLE galaxy (
+galaxy_id SERIAL PRIMARY KEY,
+name VARCHAR(200),
+description TEXT,
+age_in_millions_of_years NUMERIC(10,2),
+number_of_planets INT
 );
+
+ALTER TABLE planet ADD CONSTRAINT planet_galaxy_id_fkey FOREIGN KEY (galaxy_id) REFERENCES galaxy(galaxy_id) ON DELETE CASCADE;
+ALTER TABLE star ADD CONSTRAINT star_galaxy_id_fkey FOREIGN KEY (galaxy_id) REFERENCES galaxy(galaxy_id) ON DELETE CASCADE;
+
